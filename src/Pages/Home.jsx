cde804ccb9cb4cc71124fc15e5cededea36491bb
justsehwag <127614884+justsehwag@users.noreply.github.com>
2024-06-25 18:47:57 +0530

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,266 @@
+import { Box, Button,  Flex, Heading, HStack, Link, Tooltip, Image, Text } from '@chakra-ui/react'
+import { useToast } from '@chakra-ui/react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { GoCloudDownload } from 'react-icons/go'
+import { projects, skills } from '../Utils/data';
+import ProjectCard from '../Components/Card';
+import Svg1 from '../Components/Svg1';
+import Svg2 from '../Components/Svg2';
+import Svg3 from '../Components/Svg3';
+import Slider from 'react-slick';
+import Resume from '../Resume/SESHATHRI Y.pdf'
+
+
+const Home = () => {
+
+    const form = useRef();
+    const toast = useToast()
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        slidesToShow: matchMedia("(max-width: 425px)").matches ? 1 : matchMedia("(max-width: 1024px)").matches ? 2 : 3,
+        slidesToScroll: 1,
+    };
+
+    useEffect(() => {
+        // * it's from Aos library to to use scroll designing
+        Aos.init()
+    }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm("service_ycs7isp", "template_vzyyci8", form.current, {
+          publicKey:"3H1lrcq57UFgSIfIZ",
+        }).then(() => {
+
+            toast({
+                position: 'top-right',
+                title: 'Email Sent ✔',
+                description: `Thank You ${form.current.from_name.value.split(" ")[0]} for the message!`,
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+            })
+
+            form.current.reset();
+        }, (error) => {
+            console.log(error.text);
+            toast({
+                position: 'top-right',
+                title: 'Email Not sent.',
+                description: "There is some error",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+            })
+        });;
+
+    };
+
+    return (
+        <Box>
+            <Box id='home'>
+                <Flex flexDirection={['column-reverse', 'column-reverse', 'row']} m="auto" justifyContent="space-around" alignItems="center" h="100%">
+                    <Box data-aos="fade-down">
+                       
+                        <Heading>Hey! <span className='themeText'>I'm</span></Heading>
+                        <div id="sp">
+                        <Box className='content'>
+                            <Heading fontSize="3.3em" className='text' data-text="Seshathri Y     "><span className='themeText'>Seshathri Y     </span></Heading>
+                        </Box>
+                        </div>
+                        <Text>I am a creative Web Developer with a passion for crafting dynamic web application</Text>
+                        <HStack className='hireMe' onClick={() => { window.open("https://drive.google.com/file/d/113ZrsdIshmbCGhEAh-VXTtcygebbPpd2/view?usp=drive_link", '_blank') }}>
+                            <a href={Resume} download="Seshathri-Y-Resume">
+                                <Button>Resume <GoCloudDownload /></Button>
+                            </a>
+                        </HStack>
+                    </Box>
+                    <Box data-aos="fade-down">
+                        <Svg1 />
+                    </Box>
+                </Flex>
+            </Box>
+
+            {/* About me */}
+
+            <Box id="aboutMe">
+                <Heading>About <span className='themeText'>me</span></Heading>
+                <Flex flexDirection={['column-reverse', 'column-reverse', 'column-reverse', 'row']} alignItems="center" h="100%">
+                    <div data-aos="fade-right">
+                        <Svg3 />
+                    </div>
+
+                    <Flex data-aos="fade-left">
+                        <Flex w="100%" gap="10%" justifyContent="center">
+                            <Image
+                                borderRadius='full'
+                                boxSize='250px'
+                                src='/portfolio-profile-pic.png'
+                                alt=' Avatar' />
+                            <Svg3 />
+                        </Flex>
+
+                        <Box>
+                        <div id='repeat'>
+                            <Text>I am an enthusiastic web developer with a strong passion for creating dynamic and responsive web applications. I specialize in both front-end and back-end development, focusing on delivering functional and user-friendly digital solutions.</Text>
+                            </div>
+                        </Box>
+                    </Flex>
+                </Flex>
+            </Box>
+
+            {/* Technical Skills section */}
+            <Box id="skills">
+                <Heading>
+                    Technical
+                    <span className="themeText"> Skills</span>
+                </Heading>
+                <Flex className='skills'>
+                    <Flex>
+                        <Heading size="lg">Front<span className='themeText'>end</span></Heading>
+                        <Box>
+                            {
+                                skills.filter((el) => el.tag === "frontend").map(skill => <Box
+                                    key={skill.id}
+                                    className="skill"
+                                    data-aos="zoom-in-up">
+                                    <Box>
+                                        <Image src={skill.icon} alt={`${skill.title} icon`} />
+                                    </Box>
+                                    <Text>{skill.title}</Text>
+                                </Box>)
+                            }
+                        </Box>
+                    </Flex>
+                    <Flex>
+                        <Heading size="lg">Back<span className='themeText'>end</span></Heading>
+                        <Box>
+                            {
+                                skills.filter((el) => el.tag === "backend").map(skill => <Box
+                                    key={skill.id}
+                                    className="skill"
+                                    data-aos="zoom-in-down">
+                                    <Box>
+                                        <Image src={skill.icon} alt={`${skill.title} icon`} />
+                                    </Box>
+                                    <Text>{skill.title}</Text>
+                                </Box>)
+                            }
+                        </Box>
+                    </Flex>
+                    <Flex>
+                        <Heading size="lg">Platforms <span className='themeText'>& Tools</span></Heading>
+                        <Box>
+                            {
+                                skills.filter((el) => el.tag === "platform").map(skill => <Box
+                                    key={skill.id} className="skill"
+                                    data-aos="zoom-in">
+                                    <Box>
+                                        <Image src={skill.icon} alt={`${skill.title} icon`} />
+                                    </Box>
+                                    <Text>{skill.title}</Text>
+                                </Box>)
+                            }
+                        </Box>
+                    </Flex>
+                </Flex>
+            </Box>
+
+
+            {/* show projects */}
+            <Box id="projects">
+                <Heading textAlign="center">Featured <span className='themeText'>Projects</span></Heading>
+                <Slider {...settings}>
+                    {
+                        projects.map((project, i) => <ProjectCard key={i} {...project} />)
+                    }
+                </Slider>
+            </Box>
+
+
+            
+
+
+            {/* Contact me */}
+            <Box id='contactMe'>
+                <Heading textAlign="center">Contact <span className='themeText'>Me</span></Heading>
+                <Flex flexDirection={["column", "column", "column", "row"]} alignItems="center">
+
+                    <Box>
+                        <Svg2 />
+                    </Box>
+
+
+                    <Box className='form-section'>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div className='inputBox'>
+                                <input type="text" name="from_name" />
+                                <span>Full Name</span>
+                            </div>
+                            <div className='inputBox'>
+                                <input type="email" name="from_mail"  />
+                                <span>Email</span>
+                            </div>
+                            <div>
+                                <textarea placeholder='Message' name="message" />
+                            </div>
+                            <input type="submit" value="Send Message" />
+                        </form>
+                        <Flex className='contact-info'>   
+                        </Flex>
+                        <Flex gap={["10px", "20px", "20px", "40px"]}>
+                            
+                            <Link href='https://www.linkedin.com/in/seshathri-y' target="_blank">
+                                <Tooltip label='Seshathri Y'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://openvisualfx.com/wp-content/uploads/2019/10/linkedin-icon-logo-png-transparent.png" alt='Linkedin brand logo' />
+                                        </Box>
+                                    </Box>
+                                </Tooltip>
+                            </Link>
+
+                            <Link href="https://github.com/justsehwag" target="_blank">
+                                <Tooltip label='Seshathri Y'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt='Github brand logo' />
+                                        </Box>
+                                    </Box>
+                                </Tooltip>
+                            </Link>
+
+                            <Link href="mailto:2116114@saec.ac.in" target="_blank">
+                                <Tooltip label='Seshathri Y'>
+                                    <Box className='social-icons'>
+                                        <Box>
+                                            <Image w="100%" src="https://1000logos.net/wp-content/uploads/2021/05/Gmail-logo.png" alt='Gmail brand logo' />
+                                        </Box>
+                                    </Box >
+                                </Tooltip>
+                            </Link>
+                        </Flex >
+                    </Box >
+                </Flex >
+            </Box >
+
+            {/* footer */}
+            <Flex id='footer'>
+                <Text>© Portfolio by Seshathri-Y. | All rights reserved.</Text>
+                
+            </Flex>
+        </Box >
+    )
+}
+
+export default Home
---
+++ src/Utils/data.js
@@ -0,0 +1,110 @@
+export const skills = [
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+        title: "HTML",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+        title: "CSS",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "backend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+        title: "react",
+        tag: "frontend"
+    },
+   
+  
+    {
+        icon: "https://img.icons8.com/fluency/512/node-js.png",
+        title: "Node.js",
+        tag: "backend"
+    },
+  
+
+    {
+        icon: "https://img.icons8.com/color/512/mongodb.png",
+        title: "MongoDB",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/?size=512&id=33039&format=png",
+        title: "AWS",
+        tag: "backend"
+    },
+   
+    {
+        icon: "https://img.icons8.com/color/512/bootstrap.png",
+        title: "Bootstrap",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+        title: "Github",
+        tag: "platform"
+    },
+    {
+        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+        title: "VS Code",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/git.png",
+        title: "Git",
+        tag: "platform"
+    },
+   
+    {
+        icon: "https://img.icons8.com/color/512/firebase.png",
+        title: "Firebase",
+        tag: "platform"
+    },
+  
+
+]
+
+export const projects = [
+    {
+        id: 0,
+        title: 'Pass-Gen',
+        type: 'Security',
+        image: 'https://img.nerdburglars.net/wp-content/uploads/2021/02/password-generator-e1612432880365.jpg',
+        description: 'PassGen is a sleek, user-friendly password generator built with React. It allows users to create strong, secure passwords with customizable options .',
+        techStack: ['React', 'HTML','CSS'],
+        github: 'https://github.com/justsehwag/pass-gen',
+        liveDemo: 'https://pass-gen-justsehwag-sehwags-projects.vercel.app/'
+    },
+    {
+        id: 1,
+        title: 'Block-Fund',
+        type: 'Crypto',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpnDHXziV-cYWZwGsDsQLgzjuWnb_jza20ew&s',
+        description: "A React and Solidity-powered crowdfunding platform revolutionizing fundraising with blockchain technology.",
+        techStack: ['React', 'Solidity', 'thirdweb', 'Tailwind', 'Metamask'],
+        github: 'https://github.com/justsehwag/BlockFund',
+        liveDemo: 'https://blockfund-justsehwag-sehwags-projects.vercel.app/'
+    },
+    {
+        id: 2,
+        title: "Portfolio",
+        type: "Personal",
+        image: "https://static.ucraft.ai/fs/user_files/15696/media/images/Create-an-Online-Portfolio-Benefits-and-Building-Tips.webp",
+        description: "A clean and functional portfolio built using React, featuring my projects and skills in an easy-to-navigate format.",
+        techStack: ["React", "CSS", "HTML"],
+        github: "",
+        liveDemo: ""
+    },
+    
+        
+]
